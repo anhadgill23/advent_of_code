b72218951d609f368c9efa50de6a520883ee5cc5
Anhad Gill <anhad@frogplum.com>
2018-12-12 16:19:23 -0800

    Solution for 5b added.

--- day_5/ex_5b.rb
+++ day_5/ex_5b.rb
@@ -4,9 +4,22 @@ alphabets = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 alphabet_count = {}
 
 alphabets.each do |n|
-  dummy_string = test_string
-  dummy_string.delete_if { |a| a == n }
-  dummy_string[n] = dummy_string.size
+  dummy_string = test_string.dup
+  dummy_string.delete_if { |a| a.downcase == n }
+
+  index = 0
+
+  until index == dummy_string.size - 1
+    if (dummy_string[index].ord - dummy_string[index + 1].ord).abs == 32
+      dummy_string.delete_at(index)
+      dummy_string.delete_at(index)
+      index -= 1 unless index == 0
+    else
+      index += 1
+    end
+  end
+
+  alphabet_count[n] = dummy_string.length
 end
 
-puts alphabet_count
+puts "The answer is #{alphabet_count.values.min}"
